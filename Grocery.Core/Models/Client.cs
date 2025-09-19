4dd21d03a728ac32339954a79929658ceddb77ee
Ryan <ryan.b.a@live.nl>
2025-09-19 19:54:54 +0200

    # UC06 Inloggen In deze feature worden de functionaliteiten van het inloggen afgerond.
    ** Client.cs **
    - Setters verwijderd van _password
    - Getter toegevoegd aan _emailAddress
    - readonly properties Email en Password aangemaakt.
    
    ** ClientRepository.cs **
    - Get client functie aangemaakt. Deze zoekt de client door middel van de email.
    - Get Client functie aangemaakt. Deze zoekt de client door middel van de id.
    
    ** AuthService.cs **
    Login functie aangepassingen
    1. Eerst probeert het de ingevoerde email op te zoeken en zet het dan in de client variabel. Als het niks vindt returned het null en stopt de functie.
    2. Als de email bestaat wordt het ingevoerde wachtwoord geverifieerd met de gehashte wachtwoord. als het wachtwoord overeenkomt met de gehashte wachtwoord wordt de client gereturned. zo niet wordt null terug gereturned.
    
    *Views*
    - LoginViews.xaml en LoginViews.xaml.cs verplaatst naar Views.
    - App.xaml.cs comments weg gehaald.
    - AppShell.xaml.cs comments weg gehaald.
    - MauiProgram.cs comments weg gehaald

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -4,11 +4,14 @@ namespace Grocery.Core.Models
     public partial class Client : Model
     {
         private string _emailAddress;
-        private string _password { get; set; }
+        private string _password;
+        
+        public string Email => _emailAddress;
+        public string Password => _password;
         public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
-            _emailAddress=emailAddress;
-            _password=password;
+            _emailAddress = emailAddress;
+            _password = password;
         }
     }
 }
